a1495f46ff6a61bca81589e3d402c883fa098d2a
Bruno Belchior <brunovicenteb@gmail.com>
2023-03-16 09:15:45 -0300

    🐛 BugFix on decimal format.

---
+++ src/Toolkit/Decimals.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace Mttechne.Toolkit;
+
+public static class Decimals
+{
+    public static string ToEnglishString(this decimal value)
+        => $"$ {value:0.00}";
+}
